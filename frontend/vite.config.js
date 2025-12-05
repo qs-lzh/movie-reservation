0aae7ade94c676395d312b2317a586307b6cb95a
qs-lzh <liuzihao@qslzh.local>
2025-12-05 22:10:13 +0800

    add https in frontend, now ready to add captcha

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,8 +16,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://localhost:4000',
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
